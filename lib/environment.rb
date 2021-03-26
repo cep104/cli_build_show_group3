57b0e3a4ff0f12112993c06ff7b97f39f7750fb3
cep104 <candicelizabethpeters@gmail.com>
2021-03-26 09:41:17 -0500

    class models added

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative "show_cli_group3/version"
+# require_relative "show_cli_group3/version"
 
 require 'bundler'
 Bundler.require
 
-binding.pry
+require_rel './show_cli_group3'
