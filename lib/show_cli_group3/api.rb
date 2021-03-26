2d4fef024dd9a5c1b854f2c295b1f521d94d341a
cep104 <candicelizabethpeters@gmail.com>
2021-03-26 09:59:09 -0500

    objects made

--- lib/show_cli_group3/api.rb
+++ lib/show_cli_group3/api.rb
@@ -2,6 +2,12 @@ class API
     def self.get_data 
        response = RestClient.get('http://api.tvmaze.com/search/shows?q=girls')
        shows = JSON.parse(response)
-    #    binding.pry
+       shows.each do |show|
+        new_show = show['show']
+        # name, genres, runtime
+        Show.new(new_show["name"], new_show["genres"], new_show["runtime"])
+        # binding.pry
+       end
+        binding.pry
     end
 end
--- lib/show_cli_group3/show.rb
+++ lib/show_cli_group3/show.rb
@@ -1,3 +1,14 @@
 class Show 
-    
+    attr_accessor :name, :genres, :runtime
+    @@all = []
+    def initialize(name, genres, runtime)
+        @name = name 
+        @genres = genres 
+        @runtime = runtime
+        @@all << self
+    end
+
+    def self.all 
+        @@all 
+    end
 end
